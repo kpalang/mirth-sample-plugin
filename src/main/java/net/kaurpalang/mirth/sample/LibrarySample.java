fd83d8308f796a5039c5b1db735bb7c829e6417d
Kaur Palang <kaur.palang@protonmail.com>
2021-02-21 12:18:46 +0200

    Delete a sample class

--- src/main/java/net/kaurpalang/mirth/sample/LibrarySample.java
+++
@@ -1,8 +0,0 @@
-package net.kaurpalang.mirth.sample;
-
-import net.kaurpalang.mirth.annotationsplugin.annotation.Library;
-import net.kaurpalang.mirth.annotationsplugin.type.LibraryType;
-
-@Library(type = LibraryType.SHARED)
-public class LibrarySample {
-}
