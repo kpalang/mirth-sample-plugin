23b43fc1e9408bb64dfb9dc14e4b74f2e3633de6
Kaur Palang <>
2021-11-29 18:41:09 +0200

    Beginning of api servlets

--- shared/src/main/java/com/kaurpalang/mirthpluginsample/shared/ApiProviderSample.java
+++
@@ -1,9 +0,0 @@
-package com.kaurpalang.mirthpluginsample.shared;
-
-import com.kaurpalang.mirth.annotationsplugin.annotation.ApiProvider;
-import com.kaurpalang.mirth.annotationsplugin.type.ApiProviderType;
-
-//@ApiProvider(type = ApiProviderType.SERVLET_INTERFACE)
-public class ApiProviderSample {
-
-}
---
+++ shared/src/main/java/com/kaurpalang/mirthpluginsample/shared/MyPermissions.java
@@ -0,0 +1,8 @@
+package com.kaurpalang.mirthpluginsample.shared;
+
+public final class MyPermissions {
+
+    public static final String GETSTH = "myplugin_permission_getsth";
+
+    public MyPermissions() {}
+}
